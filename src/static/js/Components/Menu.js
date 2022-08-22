37cd8f1c3ee023f893d5d4938f7159fa4383ecec
rodrigo groff <rodrigo.groff@vortigo.com.br>
2022-08-22 09:30:21 -0300

    ajustes

--- src/static/js/Components/Menu.js
+++ src/static/js/Components/Menu.js
@@ -8,19 +8,19 @@ export default class {
     var menuItems = MenuItens.getAll();
 
     var liItem = 1350;
-    var fullMenu = '<div class="menu-main-container"><ul id="main_menu" class="nav navbar-nav group navbar-collapse collapse">';
+    var fullMenu = '<div class="menu-main-container2"><ul id="main_menu2" class="nav2 navbar2-nav2 group2 navbar2-collapse collapse2">';
     for(let a = 0; a < menuItems.items.length; ++a)
     {
       liItem++;
       var m = menuItems.items[a];
-      fullMenu += '<li id="menu-item-' + liItem + '" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-' + liItem + '">'
+      fullMenu += '<li id="menu-item-' + liItem + '" class="menu-item2 menu-item-type-post_type2 menu-item-object-page2 menu-item-has-children2 menu-item-' + liItem + '">'
       fullMenu += '<a href="' + m.link + '">' + m.label + '</a>'
       if (m.subs.length >0) {
         fullMenu += '<ul class="sub-menu">'
         for(let b = 0; b < m.subs.length; ++b) {
           var m2 = m.subs[b]
           liItem++;
-          fullMenu += '<li id="menu-item-"' + liItem + ' class="menu-item menu-item-type-post_type menu-item-object-page menu-item-' + liItem + '">'
+          fullMenu += '<li id="menu-item-"' + liItem + ' class="menu-item2 menu-item-type-post_type2 menu-item-object-page2 menu-item-' + liItem + '">'
           fullMenu += '<a style="color:white" href="' + m2.link + '">' + m2.label + '</a>'
           fullMenu += '</li>'
         }
