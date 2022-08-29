ca2e6cd2eefec547bcf34320b27208ca3fbbc24d
rodrigo groff <rodrigo.groff@vortigo.com.br>
2022-08-29 14:47:49 -0300

    ajustes

--- src/static/js/Components/ItemGallery.js
+++ src/static/js/Components/ItemGallery.js
@@ -35,11 +35,31 @@ export default class {
         carousel += `</div><a class="carousel-control left" href="#bootstrap-carousel" data-slide="prev"></a><a
         class="carousel-control right" href="#bootstrap-carousel" data-slide="next"></a>`
 
+        var buyer = `<span style='text-size:small'>Opções de compra</span><br>
+        <h4 style='color:green'>
+            <a style='text-size:large;color:green' href='https://api.whatsapp.com/send?phone=5551995152432&text=Big%20Boys%20Store - Venda PIX Item ${obj.id} - A3 150,00'>A3 - R$ 150,00</a><br>
+            Altura: 42cm, Largura: 29,7cm
+        </h4>
+        <br>
+        <h4 style='color:green'>
+            <a style='text-size:large;color:green' href='https://api.whatsapp.com/send?phone=5551995152432&text=Big%20Boys%20Store - Venda PIX Item ${obj.id} - A2 250,00'>A2 - R$ 250,00</a><br>
+            Altura: 59,4cm, Largura: 42cm
+        </h4>` 
+
+        if (obj.sculptPrice != null && obj.sculptPrice != undefined)
+        {
+            buyer = `<h4 style='color:green'>
+            <p style='text-size:large;color:green' href='https://api.whatsapp.com/send?phone=5551995152432&text=Big%20Boys%20Store - Venda PIX Mini Item ${obj.id} - ${obj.sculptPrice}'>${obj.sculptPrice}</p><br>
+            ${obj.catalogText}<br>
+            </h4>`
+        }
+
         return `<div class="container-fluid portfolio">
       <div class="container">
-          <div class="col-md-12 col-lg-12">         
+          <div class="col-md-6 col-lg-6">
                 <br>
-                <div id="myCarousel" style='padding-top:20px'>
+                <br>
+                <div id="myCarousel">
                   <div id="bootstrap-carousel" class="carousel slide ">${carousel}</div>
                   <script type="text/javascript">
                       // <![CDATA[
@@ -50,21 +70,13 @@ export default class {
                   </script>
               </div>
           </div>
-          <div class="col-md-4 col-lg-4">              
-              <div class="wcontainer pinfo" style='margin-top:20px'>
+          <div class="col-md-6 col-lg-6">
+                      <br><br>
+              <div class="wcontainer pinfo" style='height:386px'>
                 <span style='text-size:small'>Criado por:</span><br>
                 <h4 style='color:black'>${obj.patreon}</h4>
                 <br>
-                <span style='text-size:small'>Opções de compra</span><br>
-                <h4 style='color:green'>
-                    <a style='text-size:large;color:green' href='https://api.whatsapp.com/send?phone=5551995152432&text=Big%20Boys%20Store - Venda PIX Item ${obj.id} - A3 150,00'>A3 - R$ 150,00</a><br>
-                    Altura: 42cm, Largura: 29,7cm
-                </h4>
-                <br>
-                <h4 style='color:green'>
-                    <a style='text-size:large;color:green' href='https://api.whatsapp.com/send?phone=5551995152432&text=Big%20Boys%20Store - Venda PIX Item ${obj.id} - A2 250,00'>A2 - R$ 250,00</a><br>
-                    Altura: 59,4cm, Largura: 42cm
-                </h4>
+                ${buyer}                
                 <br>
                 <br>
                 <a style='color:red' href='/faq'>Clique aqui para dúvidas</a><br>
